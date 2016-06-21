32307052f227334424d16256863a0b7f1d745d70
Bjarki Þór Elvarsson <bthe@users.noreply.github.com>
2016-06-21 14:21:16 +0000

    Update tbl-sql.r

--- R/tbl-sql.r
+++ R/tbl-sql.r
@@ -3,7 +3,7 @@
 #' @export
 head.tbl_oracle <- function(x, n = 6L, ...) {
   assert_that(length(n) == 1, n > 0L)
-  dplyr::collect(x,n)
+  dplyr::collect(x,n=n)
 #  query <- build_query(x)
 #  query$sql <- build_sql('select * from  (', query$sql, ') A where ROWNUM <= ', n)
 #  query$fetch()
